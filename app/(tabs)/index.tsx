7fc7bf17c46a69198e9d31ec3fb816998362b66b
kutukmu <kutukmu@gmail.com>
2025-10-04 04:22:50 -0500

    add todos and clear todos mutaions added

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,29 @@
+import { api } from "@/convex/_generated/api";
 import useTheme from "@/hooks/useTheme";
+import { useMutation, useQuery } from "convex/react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
+  const todos = useQuery(api.todos.getTodos);
+  const addTodos = useMutation(api.todos.addTodo);
+  const clearTodos = useMutation(api.todos.clearAllTodos);
+
   const { toggleDarkMode } = useTheme();
   return (
     <View style={styles.container}>
-      <Text style={styles.content}>
-        Edit app/index.tsx to edit this screen123.
-      </Text>
-
-      <Text>Hi</Text>
+      {todos?.map(({ _id, text }) => (
+        <Text key={_id}>{text}</Text>
+      ))}
       <TouchableOpacity onPress={toggleDarkMode}>
         <Text>Toggle</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => addTodos({ text: "Hello" })}>
+        <Text>Add Todo</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => clearTodos()}>
+        <Text>Clear All Todo</Text>
       </TouchableOpacity>
     </View>
   );
